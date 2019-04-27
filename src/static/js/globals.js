b3ca1bbaa17800d6a34dbea7f65254b69ca89873
Bowe Strickland <bowe@redhat.com>
2019-04-27 09:43:19 -0400

    shift pack to prod api base

--- src/static/js/globals.js
+++ src/static/js/globals.js
@@ -1,4 +1,4 @@
 window.crowdpong_globals = {
-  api_base: "http://localhost:8000/api"
-  //api_base: "https://crowdpong.ole.redhat.com/api"
+  //api_base: "http://localhost:8000/api"
+  api_base: "https://crowdpong.ole.redhat.com/api"
 }
